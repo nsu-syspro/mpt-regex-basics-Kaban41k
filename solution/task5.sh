f34e6db7d7b760dca753064385b4f63e8658fdd3
Kaban41k <pig.anishenko@gmail.com>
2024-09-17 22:20:45 +0700

    MEGA UBER LAST UPDATE++

--- solution/task5.sh
+++ solution/task5.sh
@@ -1,1 +1,1 @@
-sed -E -e 's/,/ ,/' -e 's/([а-я]*) ([а-я]*)( .*)(\,)([0-9]{6})/\2 \1 \(\5\)/g' data/students.csv | cat > data/students.txt 
+sed -E -e 's/,/ ,/' -e 's/([^ ]*) ([а-я]*)( .*)(\,)([0-9]{6})/\2 \1 \(\5\)/g' data/students.csv | cat > data/students.txt
